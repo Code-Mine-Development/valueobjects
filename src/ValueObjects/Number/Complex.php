77fb4dd3dc9ecc884f3d4f8f0631a8509e5132e7
Nicolò Pignatelli <pignatelli.nicolo@gmail.com>
2014-02-14 15:56:14 +0100

    Add Complex number class

---
+++ src/ValueObjects/Number/Complex.php
@@ -0,0 +1,154 @@
+<?php
+
+namespace ValueObjects\Number;
+
+use ValueObjects\Util\Util;
+use ValueObjects\ValueObjectInterface;
+
+class Complex implements ValueObjectInterface, NumberInterface
+{
+    /** @var Real */
+    protected $real;
+
+    /** @var Real */
+    protected $im;
+
+    /**
+     * Returns a new Complex object from native PHP arguments
+     *
+     * @param float $real Real part of the complex number
+     * @param float $im Imaginary part of the complex number
+     * @return Complex|ValueObjectInterface
+     * @throws \BadMethodCallException
+     */
+    public static function fromNative()
+    {
+        $args = \func_get_args();
+
+        if (\count($args) != 2) {
+            throw new \BadMethodCallException('You must provide 2 arguments: 1) real part, 2) imaginary part');
+        }
+
+        $real    = Real::fromNative($args[0]);
+        $im      = Real::fromNative($args[1]);
+        $complex = new self($real, $im);
+
+        return $complex;
+    }
+
+    /**
+     * Returns a Complex given polar coordinates
+     *
+     * @param Real $modulus
+     * @param Real $argument
+     * @return Complex
+     */
+    public static function fromPolar(Real $modulus, Real $argument)
+    {
+        $realValue = $modulus->getValue() * \cos($argument->getValue());
+        $imValue   = $modulus->getValue() * \sin($argument->getValue());
+        $real      = new Real($realValue);
+        $im        = new Real($imValue);
+        $complex   = new self($real, $im);
+
+        return $complex;
+    }
+
+    /**
+     * Returns a Complex object give its real and imaginary parts as parameters
+     *
+     * @param Real $real
+     * @param Real $im
+     */
+    public function __construct(Real $real, Real $im)
+    {
+        $this->real = $real;
+        $this->im   = $im;
+    }
+
+    public function equals(ValueObjectInterface $complex)
+    {
+        if (false === Util::classEquals($this, $complex)) {
+            return false;
+        }
+
+        return $this->getReal()->equals($complex->getReal()) &&
+               $this->getIm()->equals($complex->getIm());
+    }
+
+    /**
+     * Returns the native value of the real and imaginary parts as an array
+     *
+     * @return array
+     */
+    public function getValue()
+    {
+        return array(
+            $this->getReal()->getValue(),
+            $this->getIm()->getValue()
+        );
+    }
+
+    /**
+     * Returns the real part of the complex number
+     *
+     * @return Real
+     */
+    public function getReal()
+    {
+        return $this->real;
+    }
+
+    /**
+     * Returns the imaginary part of the complex number
+     *
+     * @return Real
+     */
+    public function getIm()
+    {
+        return $this->im;
+    }
+
+    /**
+     * Returns the modulus (or absolute value or magnitude) of the Complex number
+     *
+     * @return Real
+     */
+    public function getModulus()
+    {
+        $real = $this->getReal()->getValue();
+        $im   = $this->getIm()->getValue();
+        $mod  = \sqrt(\pow($real, 2) + \pow($im, 2));
+
+        return new Real($mod);
+    }
+
+    /**
+     * Returns the argument (or phase) of the Complex number
+     *
+     * @return Real
+     */
+    public function getArgument()
+    {
+        $real = $this->getReal()->getValue();
+        $im   = $this->getIm()->getValue();
+        $arg  = \atan2($im, $real);
+
+        return new Real($arg);
+    }
+
+    /**
+     * Returns a native string version of the Complex object in format "${real} +|- ${complex}i"
+     *
+     * @return string
+     */
+    public function __toString()
+    {
+        $format = '%g %+gi';
+        $real   = $this->getReal()->getValue();
+        $im     = $this->getIm()->getValue();
+        $string = \sprintf($format, $real, $im);
+
+        return \preg_replace('/(\+|-)/', '$1 ', $string);
+    }
+}
---
+++ tests/ValueObjects/Tests/Number/ComplexTest.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace ValueObjects\Tests\Number;
+
+use ValueObjects\Number\Real;
+use ValueObjects\Tests\TestCase;
+use ValueObjects\Number\Complex;
+
+class ComplexTest extends TestCase
+{
+    /** @var Complex */
+    private $complex;
+
+    public function setup()
+    {
+        $this->complex = new Complex(new Real(2.05), new Real(3.2));
+    }
+
+    public function testFromNative()
+    {
+        $fromNativeComplex = Complex::fromNative(2.05, 3.2);
+
+        $this->assertTrue($fromNativeComplex->equals($this->complex));
+    }
+
+    public function testFromPolar()
+    {
+        $mod = new Real(3.800328933132);
+        $arg = new Real(1.0010398733119);
+        $fromPolar = Complex::fromPolar($mod, $arg);
+
+        $nativeModulus  = $this->complex->getModulus();
+        $nativeArgument = $this->complex->getArgument();
+
+        $this->assertTrue($nativeModulus->equals($fromPolar->getModulus()));
+        $this->assertTrue($nativeArgument->equals($fromPolar->getArgument()));
+    }
+
+    public function testGetValue()
+    {
+        $this->assertEquals(array(2.05, 3.2), $this->complex->getValue());
+    }
+
+    public function testGetReal()
+    {
+        $real = new Real(2.05);
+
+        $this->assertTrue($real->equals($this->complex->getReal()));
+    }
+
+    public function testGetIm()
+    {
+        $im = new Real(3.2);
+
+        $this->assertTrue($im->equals($this->complex->getIm()));
+    }
+
+    public function testGetModulus()
+    {
+        $mod = new Real(3.800328933132);
+
+        $this->assertTrue($mod->equals($this->complex->getModulus()));
+    }
+
+    public function testGetArgument()
+    {
+        $arg = new Real(1.0010398733119);
+
+        $this->assertTrue($arg->equals($this->complex->getArgument()));
+    }
+
+    public function testToString()
+    {
+        $complex = new Complex(new Real(2.034), new Real(-1.4));
+        $this->assertEquals('2.034 - 1.4i', $complex->__toString());
+    }
+}
