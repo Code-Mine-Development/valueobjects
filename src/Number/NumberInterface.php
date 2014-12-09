b3329dcc37da1b1708a852b95b6806a00efda5b5
Gabriel Schmitt <gabrielsch@outlook.com>
2014-12-09 08:39:15 -0200

    Adjusting `toNative` docblock

--- src/Number/NumberInterface.php
+++ src/Number/NumberInterface.php
@@ -4,6 +4,10 @@ namespace ValueObjects\Number;
 
 interface NumberInterface
 {
-    /** Returns a PHP native implementation of the Number value */
+    /**
+     * Returns a PHP native implementation of the Number value
+     *
+     * @return mixed
+     */
     public function toNative();
 }
