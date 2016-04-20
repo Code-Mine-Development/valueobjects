b48544467aa692297f805bdc8ca8318886b655f0
Adam Grabek <adam@grabek.io>
2016-04-20 18:12:20 +0200

    Interfaces for math added

--- src/Number/NumberInterface.php
+++ src/Number/NumberInterface.php
@@ -10,4 +10,40 @@ interface NumberInterface
      * @return mixed
      */
     public function toNative();
+
+    public function add(NumberInterface $number);
+
+    public function subtract(NumberInterface $number);
+
+    public function multiply(NumberInterface $number);
+
+    public function divide(NumberInterface $number);
+
+    public function toPower(NumberInterface $number);
+
+    public function square();
+
+    public function squareRoot();
+
+    public function cube();
+
+    public function isEven();
+
+    public function isOdd();
+
+    public function isGreaterThan(NumberInterface $number);
+
+    public function isGreaterOrEqual(NumberInterface $number);
+
+    public function isLessThan(NumberInterface $number);
+
+    public function isLessOrEqual(NumberInterface $number);
+
+    public function modulo(NumberInterface $number);
+
+    public function isZero();
+
+    public function isPositive();
+
+    public function isNegative();
 }
